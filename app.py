2605ca3ed73b17c440adfa72fa3665fff24f7d8a
Basamma Hosamani <142794839+basammahosamani@users.noreply.github.com>
2026-08-04 12:52:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import gradio as gr
 
+def chatbot(message, history):
+    return f"You asked: {message}"
+
+demo = gr.ChatInterface(
+    fn=chatbot,
+    title="🤖 AI Expert for Kids",
+    description="""
+Ask any AI question.
+The AI explains concepts for kids aged 5-15 years.
+"""
+)
+
+demo.launch()
